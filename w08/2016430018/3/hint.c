99f3be5acd27489658c8ad7aca05ed256b204ece
yssong <thddudtn01@gmail.com>
2020-10-31 04:55:58 -0400

    [W08] hint upload2

--- w08/2016430018/3/hint.c
+++ w08/2016430018/3/hint.c
@@ -12,11 +12,7 @@ typedef struct {
 } Line;
 
 float getLen(Line l){
-    float tmp[2];
-    tmp[0] = (l.p[0]->x - l.p[1]->x);
-    tmp[1] = (l.p[0]->y - l.p[1]->y);
-    tmp[0] = sqrt(pow(tmp[0],2) + pow(tmp[1], 2));
-    return tmp[0];
+    //TODO
 }
 
 int main(){
@@ -25,9 +21,7 @@ int main(){
     Point point[2];
 
     for(i = 0; i < 2; i++){
-        printf("Enter point's x, y value: ");
-        scanf("%d %d", &point[i].x, &point[i].y);
-        line.p[i] = &point[i];
+        //TODO
     }
 
     line.len = getLen(line);
