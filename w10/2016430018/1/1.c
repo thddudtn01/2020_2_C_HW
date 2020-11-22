852f7462d90e8bb01f4eddcf122c8bfb01f52c7a
yssong <thddudtn01@gmail.com>
2020-11-22 06:26:04 -0500

    typing error fixed

--- w10/2016430018/1/1.c
+++ w10/2016430018/1/1.c
@@ -2,13 +2,15 @@
 
 #define SQ1(x) x*x
 #define SQ2(x) (x)*(x)
+#define SQ3(x) ((x)*(x))
 
 int main(){
-    int x;
+    float x;
     printf("Input integer: ");
-    scanf("%d", &x);
-    printf("SQ1(x) = %d\n", SQ1(x));
-    printf("SQ2(x) = %d\n", SQ2(x));
-    printf("SQ1(x+1) = %d\n", SQ1(x+1));
-    printf("SQ2(x+1) = %d\n", SQ2(x+1));
+    scanf("%f", &x);
+    printf("SQ1(x) = %.2f\n", 1.0/SQ1(x));
+    printf("SQ2(x) = %.2f\n", 1.0/SQ2(x));
+    printf("SQ1(x+1) = %.2f\n", 1.0/SQ1(x+1));
+    printf("SQ2(x+1) = %.2f\n", 1.0/SQ2(x+1));
+    printf("SQ3(x+1) = %.2f\n", 1.0/SQ3(x+1));
 }
