0d5ce8eb8cb1f8591064dbe672050b05d7854a23
yssong <thddudtn01@gmail.com>
2020-11-22 22:55:07 -0500

    w10 example

---
+++ w10/preprocessor.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#define test1 0
+
+int main(){
+#if 0
+    printf("1\n");
+#endif
+#if test
+    printf("2\n");
+#else
+    printf("3\n");
+#endif
+#ifdef test1
+    printf("4\n");
+#endif
+#ifdef test2
+    printf("5\n");
+#endif
+}
