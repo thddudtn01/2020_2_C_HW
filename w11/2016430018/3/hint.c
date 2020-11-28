e64003c81157c4854225e97654f8e50eb5eae494
thddudtn01 <thddudtn01@gmail.com>
2020-11-28 16:44:39 +0900

    typing error fixed

--- w11/2016430018/3/hint.c
+++ w11/2016430018/3/hint.c
@@ -4,9 +4,7 @@ int main(){
     stack stk1, stk2, stk3;
     data x;
     int attachIdx, i;
-    initialize(&stk1);
-    initialize(&stk2);
-    initialize(&stk3);
+    //TODO
 
     while(1){
         printf("Input positive integer for stk1(-1 to exit): ");
@@ -24,14 +22,11 @@ int main(){
     printf("Input index of skt1 to attach skt2: ");
     scanf("%d", &attachIdx);
 
-    int tmpCnt = stk1.cnt - attachIdx;
     // After some stk1 is pushed to stk3, all stk2 is pushed.
     // Push stk3 back to stk1.
-    for(i = 0; i < tmpCnt; i++) //TODO
-    while(!empty(&stk2)) //TODO
-    while(!empty(&stk3)) //TODO
+    //TODO
 
     printf("you entered:");
-    //TODO hint : w10/1.c
+    //TODO
     printf("\n");
 }
