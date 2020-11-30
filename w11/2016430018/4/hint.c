66c5f99f56a8cae02a9928a63802d351aae21602
thddudtn01 <thddudtn01@gmail.com>
2020-11-30 13:20:07 +0900

    w11/4 hint updated

---
+++ w11/2016430018/4/hint.c
@@ -0,0 +1,30 @@
+#include "queue.h"
+
+int main(){
+    queue q;
+    data x;
+    int i, removeIdx, tmp;
+    //TODO
+
+    while(1){
+        printf("Input positive integer (-1 to exit): ");
+        scanf("%d", &x);
+        if(x < 0) break;
+        //TODO
+    }
+
+    printf("Input index of queue to remove: ");
+    scanf("%d", &removeIdx);
+    //TODO
+
+    printf("you entered:");
+    //TODO
+    printf("\n");
+}
+
+/*
+0 1 2 3 4
+2 3 4 0 1
+3 4 0 1
+0 1 3 4
+*/
